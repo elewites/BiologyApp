3d0455db17e28620dd2fea73bd9418c10967008a
Eros <elewites@gmail.com>
2021-11-22 10:13:47 -0800

    final commit before deployment

--- src/Helpers/QuestionBank.js
+++ src/Helpers/QuestionBank.js
@@ -46,7 +46,7 @@ export const Questions = [
   },
   {
     prompt:
-      "The following question tests your understanding of levels of" +
+      "The following question tests your understanding on the levels of" +
       " expression using the mal operon as an example." +
       " In the presence of regulatory protein MalT and the" +
       " sugar molecule maltose, that the mal operon will produce" +
@@ -62,11 +62,87 @@ export const Questions = [
       "Based on the following information: in the Freeman Biological " +
       " Science textbook a cistron is defined as a section of a DNA" +
       " or RNA molecule that codes for a specific polypeptide in protein" +
-      " synthesis. ",
+      " synthesis. Is the following statement true or false: " +
+      "Divergent genes are a defining characteristic of polycistronic mRNA. ",
     optionA: "True",
     optionB: "False",
     optionC: "",
     optionD: "",
     answer: "B",
+  },
+  {
+    prompt:
+      "Given your understanding of polycistronic mRNA and operons," +
+      " determine if the following statement is true or false: " +
+      " In a bacteria cell, an operon containing multiple coding" +
+      " regions also has multiple +1 sites, one for transcribed" +
+      " region of a gene",
+    optionA: "True",
+    optionB: "False",
+    optionC: "",
+    optionD: "",
+    answer: "B",
+  },
+  {
+    prompt:
+      " Given what you know about maltose, activators, inducers," +
+      " and conformational changes in regulatory proteins," +
+      " determine if the following statement is true or false:" +
+      " In the presence of maltose, the mal operon undergoes a" +
+      " conformational change that allows it to produce MalZ," +
+      " MalY, and MalA proteins.",
+    optionA: "True",
+    optionB: "False",
+    optionC: "",
+    optionD: "",
+    answer: "B",
+  },
+  {
+    prompt:
+      " In class, we learned how positive regulation differs" +
+      " from negative regulation. Although the two mechanism share" +
+      " similar components, the ways in which these" +
+      " components interact vary." +
+      " In negative regulation, where would the repressor bind" +
+      " to the promoter of an operon? ",
+    optionA:
+      "To the promoter of the operon located upstream of the coding region",
+    optionB:
+      "To the promoter of the operon located downstream of the coding region",
+    optionC:
+      "To the operator region located downstream of the operon’s promoter",
+    optionD: "To the operator region located upstream of the operon’s promoter",
+    answer: "C",
+  },
+  {
+    prompt:
+      "Given what you know about operons and operon mutations," +
+      " determine if the following statement is true or false: " +
+      " If there is a mutation in the operator region of the mal operon," +
+      " in the presence of maltose, the level of transcription" +
+      " of malPQ will be high. ",
+    optionA: "True",
+    optionB: "False",
+    optionC: "",
+    optionD: "",
+    answer: "B",
+  },
+  {
+    prompt:
+      "Which of the following most accurately describes the differences" +
+      " between positive and negative regulation?",
+    optionA:
+      "Positive regulation occurs in the mal operon and" +
+      " negative regulation occurs in the lac operon",
+    optionB:
+      "Positive regulation is a translation gene" +
+      " regulation mechanism while negative regulation only" +
+      " occurs at post-translational levels",
+    optionC:
+      "Positive regulation increases transcription while" +
+      " negative regulation decreases transcription",
+    optionD:
+      "Both positive and negative regulation involve regulatory proteins ",
+    answer: "C",
   },
 ];
